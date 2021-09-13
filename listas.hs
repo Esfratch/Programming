1c32935473fca3644a6b09907ee651eb8bac3b99
Esfratch <esfratch@gmail.com>
2021-09-13 15:21:44 -0500

    Añado archivos

--- listas.hs
+++ listas.hs
@@ -21,3 +21,29 @@ mapear f (x:xs) = f x : mapear f xs
 plegarI :: (a -> a -> a) -> a -> [a] -> a
 plegarI _ e [] = e
 plegarI f e (x:xs) = plegarI f (f e x) xs
+
+replica :: Int -> a -> [a]
+replica 0 _ = []
+replica n e = e : replica (pred n) e
+
+toma :: Int -> [a] -> [a]
+toma 0 _ = []
+toma _ [] = []
+toma n (x:xs) = x : toma (pred n) xs
+
+tirar :: Int -> [a] -> [a]
+tirar 0 xs = xs
+tirar _ [] = []
+tirar n (_:xs) = tirar (pred n) xs
+
+comprimir :: [a] -> [b] -> [(a , b)]
+comprimir [] _ = []
+comprimir _ [] = []
+comprimir (x:xs) (y:ys) = (x , y) : comprimir xs ys
+
+comprimircon :: (a -> b -> c) -> [a] -> [b] -> [c]
+comprimircon _ [] _ = []
+comprimircon _ _ [] = []
+comprimircon f (x:xs) (y:ys) = f x y : comprimircon f xs ys
+
+fib = 0 : 1 : zipWith (+) fib (tail fib)
